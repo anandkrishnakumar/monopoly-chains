628eb00c5e6b8ff2a6fc54a56eaaba068590eb50
Anand <anandkrishnakumar123@gmail.com>
2021-07-11 16:38:35 +0300

    plots added

--- CompProbs.py
+++ CompProbs.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python3
 
-#TODO
-# Add Jail
-# Dynamic plots
-
 # Import the random package
 import random
 
+import numpy as np
 # Import the matplotlib pyplot package
-import matplotlib.pyplot as pyplot
+import matplotlib.pyplot as plt
 
-# A function to generate two random numbers within the range 1 <= i <= 6 and 
-# add them together
+# A function to generate two random numbers within the range 1 <= i <= 6 and add them together
 def rollTwoDice():
     return random.randint(1,6) + random.randint(1,6)
 
@@ -97,13 +93,14 @@ class MonopolySimulation:
 
         # A list to contain the total value rolled.
         self.counters=[0.]*self.nsquares
-        self.just_visiting = 0
 
         # A variable to hold the current position
         self.currentPosition = 0
         
-        # A variable to keep track if in jail
+        # to keep track if in jail
         self.inJail = 0
+        self.jail = 0
+        self.just_visiting = 0
 
     #-----------------------------------------------
 
@@ -132,6 +129,7 @@ class MonopolySimulation:
         # These are all commands to go to a square
         communityChestResults += [ (0, 0) ] # Advance to GO
         communityChestResults += [ (0, 10) ] # Go to Jail
+        communityChestResults += [ (2, 0) ] # Pay 10 pounds or take a chance
 
         # Now create the deck of cards
         self.communityChestDeck = Deck(16, communityChestResults) # There are sixteen cards in the deck.
@@ -143,12 +141,16 @@ class MonopolySimulation:
         # roll the dice
         totalValue = rollTwoDice()
         
-        if self.inJail == 1:
+        if self.inJail in [1, 2]:
             if totalValue < 12:
                 self.inJail += 1
+                self.jail += 1
                 return
-                
-                
+        elif self.inJail == 3:
+            if totalValue < 12:
+                self.inJail = 0
+                self.just_visiting += 1
+                return
 
         # Move the player to the next position
         self.currentPosition = self.currentPosition + totalValue
@@ -161,9 +163,9 @@ class MonopolySimulation:
         assert self.currentPosition >= 0 and self.currentPosition < self.nsquares, "Error: current position is out of range."
 
         # Count the current position on the board
-        self.counters[self.currentPosition] = self.counters[self.currentPosition] + 1.
         if self.currentPosition == 10:
             self.just_visiting += 1
+        self.counters[self.currentPosition] = self.counters[self.currentPosition] + 1.
 
     #-----------------------------------------------
 
@@ -187,6 +189,16 @@ class MonopolySimulation:
             self.counters[self.currentPosition] = self.counters[self.currentPosition] + 1.    # Count the current position
             return
 
+        # A command to take a chance
+        elif typeOfAction == 2:
+            if random.random() > 0.5: # Player chooses chance 1/2 the time at random
+                self.evaluateCard(self.chanceDeck.draw())
+            return
+        
+        if self.currentPosition == 10:
+            self.inJail = 1
+            self.jail += 1
+
         assert False, "Error: card action %d is out of range" % typeOfAction
 
     #-----------------------------------------------
@@ -206,17 +218,18 @@ class MonopolySimulation:
                 return
 
         # Check if the player should go to jail
-        if self.currentPosition == 20:
+        if self.currentPosition == 30:
             self.currentPosition = 10 # Send them to the jail square
-            self.counters[self.currentPosition] = self.counters[self.currentPosition] + 1.    # Count the current position
             self.inJail = 1
+            self.jail += 1
+            self.counters[self.currentPosition] = self.counters[self.currentPosition] + 1.    # Count the current position
             return
 
     #-----------------------------------------------
 
     def play(self, nRolls):
         # Print a message
-        print("Rolling two dice " + str(nRolls) + " times...")
+        # print("Rolling two dice " + str(nRolls) + " times...")
 
         # Roll the dice
         for i in range(nRolls):
@@ -228,25 +241,93 @@ class MonopolySimulation:
             # and count the square that they land on.
             self.checkAction()
 
-if __name__ == "__main__":
+def simulate(nRolls=1000000):
     # Set the number of rolls
-    nRolls = 1000000
+    nRolls = nRolls
     m = MonopolySimulation()
     m.play(nRolls)
-    counters = m.counters
+    counters = np.array(m.counters)
+    counters[30] = m.jail
+    counters[10] = m.just_visiting
     
     # Total probability is always defined as 1.
     # Therefore, have to divide by the total number of counted values.
-    normalise(counters)
+    # normalise(counters)
+    counters /= nRolls
     
-    # Now print out the probabilities for each of the combinations
-    print("The probabilities of landing on a given Monopoly square after " + str(nRolls) + " rolls")
-    for i in range(len(counters)):
-        # Need to add one, since Python counts from zero.
-        print(" P("+str(i)+")="+str(counters[i]))
-    print("where P(n) is the probability of landing on the nth Monopoly board square")
+    # # Now print out the probabilities for each of the combinations
+    # print("The probabilities of landing on a given Monopoly square after " + str(nRolls) + " rolls")
+    # for i in range(len(counters)):
+    #     # Need to add one, since Python counts from zero.
+    #     print(" P("+str(i)+")="+str(counters[i]))
+    # print("where P(n) is the probability of landing on the nth Monopoly board square")
     
-    print("\nP(Just Visiting)", m.just_visiting/nRolls)
+    # # Create a bar chart display
+    # plot(range(len(counters)),counters)
+    
+    return counters
 
-    # Create a bar chart display
-    plot(range(len(counters)),counters)
+def n_rolls_prob(n):
+    simulation_probs = np.zeros((10000, 40))
+    for i in range(10000):
+        simulation_probs[i] = simulate(n)
+    # AFTER n DICE ROLLs
+    return np.mean(simulation_probs, 0)
+
+
+#Lets rank our probabilities for the tiles
+Names = ['Go','Old Kent Road','Chest 1','Whitechapel Road','Income Tax','Station 1',
+          'The Angel Islington','Chance 1','Euston Road','Pentonville Road','Just Visiting',
+          'Pall Mall','Utility 1','Whitehall','Northumberland Avenue','Station 2',
+          'Bow Street','Chest 2','Marlborough Street','Vine Street','Free Parking',
+          'Strand','Chance 2','Fleet Street','Trafalgar Square','Station 3','Leicester Squre',
+          'Coventry Street','Utility 2','Picadilly','Jail','Regent Street','Oxford Street',
+          'Chest 3','Bond Street','Station4','Chance 3','Park Lane','Super Tax','Mayfair']
+
+Colours = ['lavender','tab:brown','lavender','tab:brown','lavender','k','c','lavender','c','c','lavender','m','lavender','m','m',
+            'k','tab:orange','lavender','tab:orange','tab:orange','lavender','r','lavender','r','r','k','yellow',
+            'yellow','lavender','yellow','lavender','g','g','lavender','g','k','lavender','b','lavender','b']
+
+##################################### n turns plots
+
+# fig, ax = plt.subplots(2, 2)
+# fig.set_size_inches(34.5, 21.5)
+
+# probs = n_rolls_prob(1)
+# ax[0, 0].bar(range(len(probs)),probs,color=Colours)
+# ax[0, 0].set_xticks(range(len(probs)))
+# ax[0, 0].set_xticklabels(Names, rotation=90, fontsize=22)
+# ax[0, 0].get_yaxis().set_visible(False)
+
+# probs = n_rolls_prob(2)
+# ax[0, 1].bar(range(len(probs)),probs,color=Colours)
+# ax[0, 1].set_xticks(range(len(probs)))
+# ax[0, 1].set_xticklabels(Names, rotation=90, fontsize=22)
+# ax[0, 1].get_yaxis().set_visible(False)
+
+# probs = n_rolls_prob(3)
+# ax[1, 0].bar(range(len(probs)),probs,color=Colours)
+# ax[1, 0].set_xticks(range(len(probs)))
+# ax[1, 0].set_xticklabels(Names, rotation=90, fontsize=22)
+# ax[1, 0].get_yaxis().set_visible(False)
+
+# probs = n_rolls_prob(4)
+# ax[1, 1].bar(range(len(probs)),probs,color=Colours)
+# ax[1, 1].set_xticks(range(len(probs)))
+# ax[1, 1].set_xticklabels(Names, rotation=90, fontsize=22)
+# ax[1, 1].get_yaxis().set_visible(False)
+
+# fig.tight_layout()
+# fig.savefig("4turns_probs.pdf", bbox_inches='tight', pad_inches = 0)
+    
+##################################### FINAL PLOT
+probs = simulate()
+fig, ax = plt.subplots()
+fig.set_size_inches(16.5, 10.5)
+ax.bar(range(len(probs)), probs, color=Colours)
+ax.set_xticks(range(len(probs)))
+ax.set_xticklabels(Names, rotation=90, fontsize=22)
+ax.tick_params(axis='y', labelsize=22)
+
+fig.tight_layout()
+fig.savefig("comp_probs.pdf", bbox_inches='tight', pad_inches=0)
